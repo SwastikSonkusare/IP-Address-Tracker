3f6a7b4cf2e8fd978f309acdeae995f24128b5e5
Swastik <swastik.sonkusare18@gmail.com>
2021-07-19 11:15:53 +0530

    added: header-section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import headerbg from "../../assets/images/pattern-bg.png";
+
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={headerbg} alt="Header bg"></img>
+    </header>
+  );
+};
+
+export default Header;
